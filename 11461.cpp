5d84e12101088b63147fe93380961dc3d2832426
lulusnack <45396092+lulusnack@users.noreply.github.com>
2019-12-13 00:27:28 +0800

    Add files via upload

---
+++ 11461.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int input1 = 0, input2 = 0, sum = 0;
+	while (cin >> input1 >> input2)
+	{
+		if (input1 == 0 && input2 == 0)
+			break;
+		sum = 0;
+		for (int i = input1; i <= input2; i++)
+		{
+			double squareRoot = sqrt((double)i);
+			int buffer = squareRoot;
+			if (squareRoot == (double)buffer)
+				sum++;
+		}
+		cout << sum << endl;
+	}
+	return 0;
+}
